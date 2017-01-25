a2faafa8a780016bafdb736d979131370897c0f1
Chris Crutchfield <ccrutchf@ucsd.edu>
2017-01-24 18:12:03 -0800

    improved detection of prereqs

--- course-scraper/src/CourseCatalogScraper.ts
+++ course-scraper/src/CourseCatalogScraper.ts
@@ -5,30 +5,37 @@ import * as cheerio from "cheerio";
 import * as WebRequest from 'web-request';
 
 class Course {
-    constructor (public name: string, public prerequisites: string[]) { }
+    constructor (public name: string, public prerequisites: string[][]) { }
 
     public toString(): string {
-        return `${this.name} Requires [${this.prerequisites}]`;
+        function prereqsToString(prereqs: string[][]) {
+            return _.map(prereqs, p => '[' + p.join(',') + ']').join(',');
+        }
+
+        return `${this.name} Requires [${prereqsToString(this.prerequisites)}]`;
     }
 
     public static parse(name: string, description: string) {
-        let nameRegexResult = name.match(/[A-Z]{3,4} [1-9][0-9A-Z]*(?=\.)/);
-        let descriptionRegexResult = description.match(new RegExp("(<strong class=\"italic\">Prerequisites:</strong>).+"));
+        let nameRegexResult = name.match(/[A-Za-z]{3,4}\s[1-9][0-9A-Z]*(?=\.)/);
+        let descriptionRegexResult = description.match(new RegExp("(<strong class=\"italic\">Prerequisites:</strong>)(.|\\s)+"));
 
         // Exit if our data was invalid.
         if (nameRegexResult === null || descriptionRegexResult == null) return undefined;
 
         // Transform the prerequisites text into something useful.
-        let prereqs = _.map(
-            _.without(
-                _.map(descriptionRegexResult[0].substring(47).split(','),
-                p => p.match(/[A-Z]{3,4} [1-9][0-9A-Z]*/)),
-            null),
-        p => p[0]);
+        let prereqs = Course.parsePrereqs(descriptionRegexResult);
 
         let course = new Course(nameRegexResult[0], prereqs);
 
         return course;
+    }
+
+    private static parsePrereqs(regexResult: RegExpMatchArray) {
+        let potentialPrereqs = regexResult[0].substring(47).split(',');
+        let matchesWithNull = _.map(potentialPrereqs, p => p.match(/[A-Za-z]{3,4}\s[1-9][0-9A-Z]*/g));
+        let matchesWithoutNull = _.without(matchesWithNull, null);
+
+        return _.map(matchesWithoutNull, m => m.map(ma => ma));
     }
 }
 
