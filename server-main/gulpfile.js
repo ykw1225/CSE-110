ef8d947acc59cbaf5c38ee7efd0f996eab39ae19
Chris Crutchfield <ccrutchf@ucsd.edu>
2017-02-07 14:56:27 -0800

    Angular working

--- server-main/gulpfile.js
+++ server-main/gulpfile.js
@@ -1,15 +1,28 @@
 let gulp = require('gulp');
 
 let typescript = require('gulp-typescript');
+let using = require('gulp-using');
+let watch = require('gulp-watch');
 
 let tsconfig = require('./tsconfig.json');
 
-gulp.task('compile:ts', function () {
-    return gulp.src(['./frontend-src/**/*.ts'])
-            .pipe(typescript(tsconfig.compileOptions))
+function compileTypeScript() {
+    return gulp.src(tsconfig.include)
+            .pipe(using())
+            .pipe(typescript(tsconfig.compilerOptions))
             .pipe(gulp.dest('public'));
+}
+
+gulp.task('compile:ts', function () {
+    return compileTypeScript();
 });
 
 gulp.task('compile', ['compile:ts']);
+
+gulp.task('watch:ts', function () {
+    watch('./frontend-src/**/*.ts', compileTypeScript);
+});
+
+gulp.task('watch', ['watch:ts']);
 
 gulp.task('default', ['compile']);
