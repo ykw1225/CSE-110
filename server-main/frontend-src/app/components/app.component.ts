855f9fab0ccd8707ad38dbf67ff7e1b52a67f55a
Chris Crutchfield <ccrutchf@ucsd.edu>
2017-02-07 14:56:49 -0800

    Angular working

---
+++ server-main/frontend-src/app/components/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    template: '<p>Angular is working</p>'
+})
+export class AppComponent {
+
+}
---
+++ server-main/frontend-src/main.ts
@@ -0,0 +1,6 @@
+import '/pollyfills';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/modules/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
