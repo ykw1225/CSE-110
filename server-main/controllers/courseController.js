628bc5ece832fe8ac011c43befc48a9ce354b66b
Raghav Kansal <rkansal@ucsd.edu>
2017-02-07 18:11:35 -0800

    error handling update

--- server-main/controllers/courseController.js
+++ server-main/controllers/courseController.js
@@ -2,7 +2,7 @@ var database_accessor = require('database-accessor');
 
 //check if a string contains number
 var letters = /^[A-Za-z]+$/;
-var numCheck = /^[0-9A-Za-z]+$/; //coursenumber can contain letters and numbers
+var letterNumCheck = /^[0-9A-Za-z]+$/; //coursenumber can contain letters and numbers
 
 // This function should contain all of the course related RESTful APIs
 exports.init = function (app) {
@@ -18,48 +18,8 @@ exports.init = function (app) {
             response.status(errorNode.Code).send(errorNode.Message);
         }
 
-        //checking coursenumber params, report error if it contains not only numbers
-        else if (!(request.params.coursenumber.match(numCheck)) || request.params.coursenumber.length <=0 ) {
-            var errorNode = {
-                Code: 400,
-                Message: "incorrect_request_format: coursenumber\n"
-            };
-            response.status(errorNode.Code).send(errorNode.Message);
-        }
-        
-        else {
-        console.log("Finding Course Info For " + request.params.department + " " +
-            request.params.coursenumber);
-
-        database_accessor.getCourseInfo(request.params.department.toUpperCase() + ' '
-            + request.params.coursenumber, function (result) {
-                // Handle results
-                if (result.Code != 200){
-                    //error occurred
-                    response.status(result.Code).send(result.Message);
-                }
-                else {
-                    //return response 
-                    response.status(result.Code).send(result.body);
-                }
-            });
-        }
-    });
-
-    app.get('/api/course/map/:department/:coursenumber', function (request, response) {
-        // Handle incorrect request format.
-
-        //checking deapartment params, report error if it contains not only letters
-        if (!request.params.department.match(letters) || !request.params.department) {
-            var errorNode = {
-                Code: 400,
-                Message: "incorrect request format: department\n"
-            };
-            response.status(errorNode.Code).send(errorNode.Message);
-        }
-
-        //checking coursenumber params, report error if it contains not only numbers
-        else if (!request.params.coursenumber.match(numCheck)) {
+        //checking coursenumber params, report error if it contains something other than numbers or letters
+        else if (!(request.params.coursenumber.match(letterNumCheck)) || request.params.coursenumber.length <=0 ) {
             var errorNode = {
                 Code: 400,
                 Message: "incorrect request format: coursenumber\n"
@@ -68,18 +28,56 @@ exports.init = function (app) {
         }
 
         else {
-        console.log("Finding Course Map For " + request.params.department + " " +
+        console.log("Finding Course Info For " + request.params.department + " " +
             request.params.coursenumber);
 
-        database_accessor.getCourseMap(request.params.department.toUpperCase() + ' '
-            + request.params.coursenumber, function (result) {
+        //calling database
+        database_accessor.getCourseInfo(request.params.department.toUpperCase() + ' ' + request.params.coursenumber.toUpperCase(), function (result) {
+                // Handle results
+                if (result.Code != 200){
+                    //error occurred
+                    response.status(result.Code).send(result.Message);
+                }
+                else {
+                    //return response
+                    response.send(result.body);
+                }
+            });
+        }
+    });
+
+    app.get('/api/course/map/:department/:coursenumber', function (request, response) {
+        // Handle incorrect request format.
+
+        //checking department params, report error if it contains not only letters
+        if (!request.params.department.match(letters) || !request.params.department) {
+            var errorNode = {
+                Code: 400,
+                Message: "incorrect request format: department\n"
+            };
+            response.status(errorNode.Code).send(errorNode.Message);
+        }
+
+        //checking coursenumber params, report error if it contains something other than numbers or letters
+        else if (!request.params.coursenumber.match(letterNumCheck)) {
+            var errorNode = {
+                Code: 400,
+                Message: "incorrect request format: coursenumber\n"
+            };
+            response.status(errorNode.Code).send(errorNode.Message);
+        }
+
+        else {
+        console.log("Finding Course Map For " + request.params.department + " " + request.params.coursenumber);
+
+        database_accessor.getCourseMap(request.params.department.toUpperCase() + ' ' + request.params.coursenumber.toUpperCase(), function (result) {
                 // Handle results
                 if (result[0].hasOwnProperty('Code')){
                     //error occurred
                     response.status(result[0].Code).send(result[0].Message);
                 }
                 else {
-                    response.status(200).send(result);
+                    response.send(result);
                 }
             });
         }
@@ -93,11 +91,13 @@ exports.init = function (app) {
     });
 
     app.get('/api/:department/courses', function (request, response) {
-        console.log("Finding All courses in " + request.params.department);
+        console.log("Finding all courses in " + request.params.department);
         database_accessor.getAllClassesInDepartment(request.params.department.toUpperCase(), function(result) {
             response.send(result);
         });
     });
+
+
 
     // Deprecated.  Refactored API to make it more angular friendly.
     app.post("/getCourseInfo", function(req, res) {
