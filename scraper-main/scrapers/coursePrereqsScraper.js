1b3b5b63f4c8563b512a6232bc6ee429b9443004
Raghav Kansal <rkansal@ucsd.edu>
2017-02-13 10:20:59 -0800

    schema update, prereq scraping set up

--- scraper-main/scrapers/coursePrereqsScraper.js
+++ scraper-main/scrapers/coursePrereqsScraper.js
@@ -1,6 +1,7 @@
 
 
 exports.getPrereqs = function(callback, request, cheerio, courseArray) {
+    // courseArray = [department, number, title, description, credits, prereqs, coreqs, quarter]
     var prereqs = [];
 
     courseArray[5] = prereqs;
