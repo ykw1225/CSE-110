c7fd58d731f66dfdbedba84660b6d694820a7713
Gina <gratto@ucsd.edu>
2017-02-17 13:58:18 -0800

    Fixed style

--- server-main/static-content/js/graphController.js
+++ server-main/static-content/js/graphController.js
@@ -1,14 +1,14 @@
 var xmlhttp;
 if (window.XMLHttpRequest) {
-xmlhttp = new XMLHttpRequest();
+  xmlhttp = new XMLHttpRequest();
 } else {
 // code for older browsers
-xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 }
 xmlhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-  console.log(this.responseText);
-}
+  if (this.readyState == 4 && this.status == 200) {
+    console.log(this.responseText);
+  }
 };
 xmlhttp.open("GET", "http://localhost:3000/api/course/map/cse/100", true);
 xmlhttp.send();
