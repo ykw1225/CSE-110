b46ac8c7cf9e22c75b351c2edcb482e6e9ff9a3b
Vivian Pham <Vivian@Vivians-MacBook-Pro.local>
2017-02-17 21:34:53 -0800

    Edited right-bar component

--- server-main/frontend-src/app/components/app.rightbar.ts
+++ server-main/frontend-src/app/components/app.rightbar.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+import { CollegeService } from '../services/college.service';
+import { UndergradDegreeService } from '../services/undergraddegree.service';
+
 @Component({
     selector: 'right-bar',
     templateUrl: '/templates/rightBar.html'
 })
 export class RightBarComponent {
+/**I wasn't exactly sure what to put here so I'm leaving it blank for now.*/
 }
