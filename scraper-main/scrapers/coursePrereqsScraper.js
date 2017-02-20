3cb31c95a14378f841278ab53975f49af3ce338a
Madel Asistio <madel.asistio@gmail.com>
2017-02-19 20:44:39 -0800

    fixed preRequisite scraper bugs

--- scraper-main/scrapers/coursePrereqsScraper.js
+++ scraper-main/scrapers/coursePrereqsScraper.js
@@ -4,74 +4,39 @@ exports.getPrereqs = function(callback, request, cheerio, courseArray) {
   // courseArray = [department, number, title, description, credits, prereqs, orReqs, quarter]
   var prereqs = [];
 
-  // courseName = department + number; ex: CSE100
   var courseName = courseArray[0] + courseArray[1];
-
-  // not sure what form 'quarter' is in the courseArray, it might go in the
-  // variable 'termCode' below
   var termCode = 'SP17';
-
   var url = 'https://act.ucsd.edu/scheduleOfClasses/scheduleOfClassesPreReq.htm?termCode='
   + termCode
   +'&courseId='
   + courseName;
 
-
   request(url, function(error, response, html) {
 
-    // checks if request was successful
     if(!error){
 
       var $ = cheerio.load(html);
 
-      var selector = $('td > span.bold_text');
+      prereqs = $('td').map(function() {
 
-      // checks if selector is valid
-      if (selector.length == 0) {
+        var coReqs = []
 
-        courseArray[5] = null;
+        var data = $(this).children('span.bold_text').text().replace(/ +/g,'%');
 
-      } else {
+        if ( data != '' ) {
+          var parsed = data.split('%');
+          parsed.pop();
+          coReqs.push(parsed);
+          return coReqs;
+        } else {
+          return null;
+        }
 
-        prereqs = $('td').map(function() {
-          var orReqs = []
-
-          // grabs all of the bolded text inside each row, and replaces the spaces
-          // in between with a delimiter '%' to parse. Might fail if there
-          // is a class without a space after the name.
-          var data = $(this).children('span.bold_text').text().replace(/ +/g,'%');
-
-          // filters out rows without class names
-          if ( data != '' ) {
-
-            // saves the parsed data in an array
-            var parsed = data.split('%');
-
-            // removes the last element in parsed, which is an empty string
-            parsed.pop();
-
-            // pushes all the 'or' classes into an array
-            orReqs.push(parsed);
-
-            return orReqs;
-
-          } else {
-
-            return null;
-          }
-
-        }).get();
-
-        courseArray[5] = prereqs;
-
-      }
-
-      // if error, sets prereqs to null
-    } else {
-
-      courseArray[5] = null;
-
+      }).get();
     }
 
+    courseArray[5] = prereqs;
     callback(courseArray);
-  };
+
+  }) ;
+}
