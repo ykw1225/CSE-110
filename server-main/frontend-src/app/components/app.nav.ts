b056a2c28f145188bb205d400144b93c1cd9c17a
Chris Crutchfield <ccrutchf@ucsd.edu>
2017-03-02 17:33:10 -0800

    added missing files

---
+++ server-main/frontend-src/app/components/app.nav.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'nav',
+    templateUrl: '/templates/nav.html'
+})
+export class NavComponent {
+}
---
+++ server-main/frontend-src/app/components/app.toolbar.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'toolbar',
+    templateUrl: '/templates/toolbar.html'
+})
+export class ToolbarComponent {
+}
