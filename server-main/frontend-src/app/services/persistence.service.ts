28e2b8e8bcf3d288ae07075fed57a6d52c9fb39c
KIT <ykw005@ucsd.edu>
2017-03-07 23:13:27 -0800

    UPDATE: Styling, changed svaeData to addData, and added setData

--- server-main/frontend-src/app/services/persistence.service.ts
+++ server-main/frontend-src/app/services/persistence.service.ts
@@ -6,69 +6,74 @@ export class PersistenceService{
 	constructor(){};
 
 	// To store a single data locally
-	public saveData(key: string, data: Object){
+	public addData(key: string, data: Object){
 
 		// check if key or data is null
 		if(key !== null && data !== null){
 
-			var replaced: boolean = false;
-
 			// check if the data is already existed
 			if(localStorage.getItem(key)){
 				console.log("\tWARNNING: [KEY](" + key + ") is already EXISTED with [DATA](" + localStorage.getItem(key) + ")");
-				console.log("\tWARNNING: [KEY](" + key + ") is REPLACED with [DATA](" + JSON.stringify(data) + ")");
-				replaced = true;
+
+				/*
+				 * // I am not sure if this part is neccessary
+				 * console.log("\t\tREPLACE with new value [y for yes, otherwise no] : ");
+				 * var answer;
+				 * // assign input to answer;
+				 * if( answer === "y" || answer === "y")
+				 */
+				this.setData(key, data);
 			}
 
-			localStorage.setItem(key, JSON.stringify(data));
+			else{
+				localStorage.setItem(key, JSON.stringify(data));
+				console.log("\tSUCCEED: to add [KEY](" + key + ") with [DATA](" + JSON.stringify(data) + ")");
+			}
 
-		}else{
-			if(replaced) console.log("\tSUCCEED to add [KEY](" + key + ") with [DATA](" + JSON.stringify(data) + ")");
-			else console.log("\tERROR: Either [KEY] or [DATA] is INVALID !!!");
-		}
+		}else console.log("\tERROR: Either [KEY] or [DATA] is INVALID !!!");
 
 	} // end of saveData
 
 	// To delete a single data from local by given KEY
 	public deleteData(key: string){
 
-		// check if KEY is null
-		if(key === null){
-			console.log("\tERROR:[KEY] is inValid !!!");
-			return;
-		}
-
 		// remove the data
 		if(localStorage.getItem(key)){
 			localStorage.removeItem(key);
-			console.log("\tSUCCEED to remove " + key + " !!!");
+			console.log("\tSUCCEED: to remove " + key);
 		}
 
 		// the data corresponding to the KEY is not found
-		else
-			console.log("\tERROR: " + key + " is NOT FOUND !!!");
+		else console.log("\tFAILE: to find " + key);
 
 	} // end of deleteData
 
 	// To get a single data from local by given KEY
 	public getData(key: string){
 
-		// check if KEY is null
-		if(key === null){
-			console.log("\tERROR:[KEY] is inValid !!!");
-			return;
-		}
-
 		// get the value of the data
 		if(localStorage.getItem(key)){
-			console.log("\tSUCCEED: find " + key + " !!!");
+			console.log("\tSUCCEED: to find " + key);
 			return JSON.parse(localStorage.getItem(key));
 		}
 
 		// the data corresponding to the KEY is not found
-		else
-			console.log("\tERROR: " + key + " is not found !!!");
+		else console.log("\tFAILE: to find " + key);
 
 	} // end of getData
+
+	// To change a single data by given KEY
+	public setData(key: string, data: Object){
+
+		// get the value of the data
+		if(localStorage.getItem(key)){
+			localStorage.setItem(key, JSON.stringify(data));
+			console.log("\tSUCCEED to change [KEY](" + key + ") with [DATA](" + JSON.stringify(data) + ")");
+		}
+
+		// the data corresponding to the KEY is not found
+		else console.log("\tFAILE: to find " + key);
+
+	}
 
 } // end of PersistenceService
