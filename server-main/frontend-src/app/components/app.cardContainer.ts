9a2644bc79f33b43a103b607d57c9c7485b928fa
Gina <gratto@ucsd.edu>
2017-03-08 20:03:30 -0800

    created cardContainer

---
+++ server-main/frontend-src/app/components/app.cardContainer.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'card-container',
+    templateUrl: '/templates/cardContainer.html'
+})
+export class CardContainerComponent {
+}
