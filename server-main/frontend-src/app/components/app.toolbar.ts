3e1241c76dafc66644368b32c14d6ef8bbe7bc51
Chris Crutchfield <ccrutchf@ucsd.edu>
2017-03-09 17:43:54 -0800

    I can shoot myself in the foot again

--- server-main/frontend-src/app/components/app.toolbar.ts
+++ server-main/frontend-src/app/components/app.toolbar.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { PubSubEventService, Events } from '../services/pubsubevent.service';
+import { UndergradDegreeService } from '../services/undergraddegree.service';
 
 import 'materialize-css';
 
@@ -9,10 +10,15 @@ import 'materialize-css';
     templateUrl: '/templates/toolbar.html'
 })
 export class ToolbarComponent {
-    constructor(private _pubsubEventService: PubSubEventService) {
+    constructor(private _pubsubEventService: PubSubEventService, private _undergradDegreeService: UndergradDegreeService) {
     }
 
     public _clearGraph(): void {
         this._pubsubEventService.publish(Events.ClearButtonEvent);
+    }
+
+    private async _shootSelfInFootAsync(): Promise<void> {
+        let payload = await this._undergradDegreeService.getDegreeAsync("Gary", "cse", "26");
+        this._pubsubEventService.publish(Events.DegreeAddedEvent, payload);
     }
 }
