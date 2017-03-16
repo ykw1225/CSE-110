612a7eaeb9a56fcd61bb8e87808e603b632a2ac6
Chris Crutchfield <ccrutchf@ucsd.edu>
2017-03-16 10:01:20 -0700

    fix dialogs

--- server-main/frontend-src/main.ts
+++ server-main/frontend-src/main.ts
@@ -4,5 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/modules/app.module';
 
+window.addEventListener("contextmenu", function(e) { e.preventDefault(); })
+
 enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
